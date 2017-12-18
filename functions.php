d627979fa59e9922caebfde142459f24d3919ebc
jens_jacobsen <knasen_jens@hotmail.com>
2017-12-18 11:21:58 +0100

    catalog options

--- functions.php
+++ functions.php
@@ -9,3 +9,11 @@ require_once 'classes/class-fl-child-theme.php';
 
 // Actions
 add_action( 'wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000 );
+
+
+// Moving the catalog section up
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
+add_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 10 );
+
+ //remove display notice - Showing all x results
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20 );
